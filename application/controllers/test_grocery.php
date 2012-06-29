460c611a98daa37b9e84a7513705b5580a10654b
Manuel Alejandro <punketo28@gmail.com>
2012-06-29 09:03:36 -0430

    Se probaron todos los metodos de la API de Grocery CRUD

--- application/controllers/test_grocery.php
+++ application/controllers/test_grocery.php
@@ -20,6 +20,68 @@ class Test_grocery extends CI_Controller{
 		$this->invocaVista($output);
 	}
 	
+	function test_add_fields(){
+		//$this->grocery_crud->set_theme("datatables");		
+		$this->grocery_crud->set_subject("Empleados");		
+		$this->grocery_crud->set_table('employees');
+		$this->grocery_crud->columns('lastName', 'firstName', 'email');
+		$this->grocery_crud->add_fields('lastName','firstName','extension');
+		$this->grocery_crud->display_as('lastName','Apellido');
+		$this->grocery_crud->display_as('firstName','Nombre');
+		$this->grocery_crud->display_as('email','Correo');
+		$this->grocery_crud->add_action('Ver Mensaje', base_url().'application/assets/images/msg.png', 'test_grocery/showMessage');
+		$output = $this->grocery_crud->render();
+		$this->invocaVista($output);
+	}
+	
+	function test_edit_fields(){	
+		//$this->grocery_crud->set_theme("datatables");		
+		$this->grocery_crud->set_subject("Empleados");		
+		$this->grocery_crud->set_table('employees');
+		$this->grocery_crud->columns('lastName', 'firstName', 'email');
+		$this->grocery_crud->edit_fields('lastName','firstName','extension');
+		$this->grocery_crud->display_as('lastName','Apellido');
+		$this->grocery_crud->display_as('firstName','Nombre');
+		$this->grocery_crud->display_as('email','Correo');
+		$this->grocery_crud->add_action('Ver Mensaje', base_url().'application/assets/images/msg.png', 'test_grocery/showMessage');
+		$output = $this->grocery_crud->render();
+		$this->invocaVista($output);
+	}
+
+	function test_required_fields(){
+		//$this->grocery_crud->set_theme("datatables");		
+		$this->grocery_crud->set_subject("Empleados");		
+		$this->grocery_crud->set_table('employees');
+		$this->grocery_crud->columns('lastName', 'firstName', 'email');
+		$this->grocery_crud->add_fields('lastName','firstName','extension');
+		$this->grocery_crud->display_as('lastName','Apellido');
+		$this->grocery_crud->display_as('firstName','Nombre');
+		$this->grocery_crud->display_as('email','Correo');
+		$this->grocery_crud->required_fields('email', 'lastName');
+		$this->grocery_crud->add_action('Ver Mensaje', base_url().'application/assets/images/msg.png', 'test_grocery/showMessage');
+		$output = $this->grocery_crud->render();
+		$this->invocaVista($output);
+	}
+	
+	function test_callback_add_field(){
+		//$this->grocery_crud->set_theme("datatables");		
+		$this->grocery_crud->set_subject("Empleados");		
+		$this->grocery_crud->set_table('employees');
+		$this->grocery_crud->columns('lastName', 'firstName', 'email');
+		$this->grocery_crud->add_fields('lastName','firstName','extension');
+		$this->grocery_crud->display_as('lastName','Apellido');
+		$this->grocery_crud->display_as('firstName','Nombre');
+		$this->grocery_crud->display_as('email','Correo');
+		$this->grocery_crud->required_fields('lastName');
+		$this->grocery_crud->callback_add_field('lastName',array($this, 'addCallbackLastname'));
+		$this->grocery_crud->add_action('Ver Mensaje', base_url().'application/assets/images/msg.png', 'test_grocery/showMessage');
+		$output = $this->grocery_crud->render();
+		$this->invocaVista($output);
+	}
+	function addCallbackLastname(){
+		return '<span style="color:ffffff">*</span> <input type="text" maxlength="50" value="" name="phone" style="width:200px">';
+	}
+		
 	function invocaVista($output){
 		$this->load->view('test_grocery', $output);
 	}
